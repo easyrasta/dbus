215e5e4fb16e145b953093df108f30f465f1cb9f
Julien Mathevet <jma@witbe.net>
2018-04-17 17:38:52 +0200

    fix dbus system address on windows plateform

--- conn_other.go
+++ conn_other.go
@@ -33,10 +33,3 @@ func getSessionBusPlatformAddress() (string, error) {
 	return addr, nil
 }
 
-func getSystemBusPlatformAddress() string {
-	address := os.Getenv("DBUS_SYSTEM_BUS_ADDRESS")
-	if address != "" {
-		return fmt.Sprintf("unix:path=%s", address)
-	}
-	return defaultSystemBusAddress
-}
